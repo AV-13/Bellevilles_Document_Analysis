fb225da138583302fb57136fb3f1996f8a77576f
Augustin V <augustinvathonne@gmail.com>
2024-01-15 13:02:56 +0100

    feat(preview): preview modal and close modal fix

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,18 +1,40 @@
 import './Modal.css';
+import React, { forwardRef, useEffect } from 'react';
 
-const Modal = ({ show, children, onClose }) => {
+const Modal = React.forwardRef(({ show, children, onClose }, modalRef) => {
+    const handleKeyDown = (event) => {
+        if (event.keyCode === 27) { // 27 est le code pour la touche Échap
+            onClose();
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+
+        // Nettoyage de l'écouteur d'événements lors du démontage du composant
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        };
+    }, []);
+    const handleBackdropClick = (event) => {
+        if (modalRef.current && !modalRef.current.contains(event.target)) {
+            onClose();
+        }
+    };
+
     if (!show) {
         return null;
     }
 
     return (
-        <div className="modal-backdrop">
-            <div className="modal">
+        <div className="modal-backdrop" onClick={handleBackdropClick}>
+            <div className="modal" ref={modalRef}>
                 {children}
                 <button onClick={onClose}>Close</button>
             </div>
         </div>
     );
-};
+});
+
 
 export default Modal;
--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(cors({
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/public', express.static('public'));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: false }));
